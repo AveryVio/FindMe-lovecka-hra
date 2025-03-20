76c137263788bfc9a66768678caf18a10e628503
AveryVi <stepan.nyrsko@gmail.com>
2025-03-20 16:56:27 +0100

    trying to extract string from args

--- docs/firmware/testingNMEAparser/main.c
+++ docs/firmware/testingNMEAparser/main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-int main( int argc, char *argv[]) {
+int* main( int argc, char *argv[]) {
 /*
     FILE *fptr;
     fptr = fopen("./test.txt", "w");
     fputs(argv[0], fptr);
     fclose(fptr);
 */
-    for (int i = 0; i < argc; i++) {
-        printf( "%s\n", argv[i]);
-    }
+    printf("%s\n", argv[1]);
+
+
+    char latitudeGNSS[10]="\0\0\0\0\0\0\0\0\0";
+    char longitudeGNSS[10]="\0\0\0\0\0\0\0\0\0";
+    printf("%s , %s\n", latitudeGNSS, longitudeGNSS);
+
+    printf("s\n");
+
+    char *GNSSstring = argv[1];
+    char dataValidGNSS = GNSSstring[42];
+    printf("%s\n", dataValidGNSS);
+
 
     printf("Hello, World!\n");
     return 0;

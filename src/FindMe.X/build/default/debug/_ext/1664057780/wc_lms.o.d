b94b251c857dc913dd4cb0a4d2b60f011370660f
AveryVi <stepan.nyrsko@gmail.com>
2025-04-04 00:03:31 +0200

    removed mplab generated files

--- src/FindMe.X/build/default/debug/_ext/1664057780/wc_lms.o.d
+++
@@ -1,8 +0,0 @@
-build/default/debug/_ext/1664057780/wc_lms.o: \
- ../src/third_party/wolfssl/wolfssl/wolfcrypt/src/wc_lms.c \
- ../src/third_party/wolfssl/wolfssl/wolfcrypt/settings.h \
- ../src/third_party/wolfssl/wolfssl/wolfcrypt/visibility.h
-
-../src/third_party/wolfssl/wolfssl/wolfcrypt/settings.h:
-
-../src/third_party/wolfssl/wolfssl/wolfcrypt/visibility.h:
